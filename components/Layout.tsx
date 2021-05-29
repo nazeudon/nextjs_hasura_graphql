532012854feedbfab5411e425394643df5328f89
nazeudon <nazeudon48112@icloud.com>
2021-05-29 14:37:33 +0900

    added Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,94 @@
+import { ReactNode, VFC } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+interface Props {
+  children: ReactNode
+  title: string
+}
+
+export const Layout: VFC<Props> = ({
+  children,
+  title = 'Welcome to Nextjs',
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen font-mono text-sm text-gray-600">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header>
+        <nav className="w-screen bg-gray-800">
+          <div className="flex items-center pl-8 h-14">
+            <div className="flex space-x-4">
+              <Link href="/">
+                <a
+                  data-testid="home-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  Home
+                </a>
+              </Link>
+              <Link href="/local-state-a">
+                <a
+                  data-testid="makevar-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  makeVar
+                </a>
+              </Link>
+              <Link href="/hasura-main">
+                <a
+                  data-testid="fetchpolicy-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  fetchPolicy(hasura)
+                </a>
+              </Link>
+              <Link href="/hasura-crud">
+                <a
+                  data-testid="crud-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  CRUD(hasura)
+                </a>
+              </Link>
+              <Link href="/hasura-ssg">
+                <a
+                  data-testid="ssg-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  SSG+ISR(hasura)
+                </a>
+              </Link>
+              <Link href="/hooks-memo">
+                <a
+                  data-testid="memo-nav"
+                  className="px-3 py-2 text-gray-300 rounded hover:bg-gray-700"
+                >
+                  custom hook + memo
+                </a>
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-col items-center justify-center flex-1 w-screen">
+        {children}
+      </main>
+      <footer className="flex items-center justify-center w-full h-12 border-t">
+        <a
+          className="flex items-center"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className="h-4 ml-2">
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
