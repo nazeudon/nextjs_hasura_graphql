0b1ff25dca4c37c9651795dd69c753de362489f2
nazeudon <nazeudon48112@icloud.com>
2021-05-31 09:54:43 +0900

    added memo in UserItem component

--- components/UserItem.tsx
+++ components/UserItem.tsx
@@ -14,39 +14,37 @@ interface Props {
   >
 }
 
-export const UserItem: VFC<Props> = ({
-  user,
-  delete_users_by_pk,
-  setEditedUser,
-}) => {
-  console.log('UserItem rendered')
+export const UserItem: VFC<Props> = memo(
+  ({ user, delete_users_by_pk, setEditedUser }) => {
+    console.log('UserItem rendered')
 
-  return (
-    <div className="my-1">
-      <span className="mr-2">{user.name}</span>
-      <span className="mr-2">{user.created_at}</span>
-      <button
-        className="px-3 py-1 mr-1 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
-        data-testid={`edit-${user.id}`}
-        onClick={() => {
-          setEditedUser(user)
-        }}
-      >
-        Edit
-      </button>
-      <button
-        className="px-3 py-1 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
-        data-testid={`delete-${user.id}`}
-        onClick={async () => {
-          await delete_users_by_pk({
-            variables: {
-              id: user.id,
-            },
-          })
-        }}
-      >
-        Delete
-      </button>
-    </div>
-  )
-}
+    return (
+      <div className="my-1">
+        <span className="mr-2">{user.name}</span>
+        <span className="mr-2">{user.created_at}</span>
+        <button
+          className="px-3 py-1 mr-1 text-white bg-green-600 hover:bg-green-700 rounded-2xl focus:outline-none"
+          data-testid={`edit-${user.id}`}
+          onClick={() => {
+            setEditedUser(user)
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="px-3 py-1 text-white bg-pink-600 hover:bg-pink-700 rounded-2xl focus:outline-none"
+          data-testid={`delete-${user.id}`}
+          onClick={async () => {
+            await delete_users_by_pk({
+              variables: {
+                id: user.id,
+              },
+            })
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    )
+  }
+)
