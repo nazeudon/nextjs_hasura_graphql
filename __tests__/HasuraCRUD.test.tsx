4888a78e54cc6d8c7beb168d48231291a58fc2b7
nazeudon <nazeudon48112@icloud.com>
2021-06-03 08:54:20 +0900

    added TEST of hasura-ssg

--- __tests__/HasuraCRUD.test.tsx
+++ __tests__/HasuraCRUD.test.tsx
@@ -1,6 +1,5 @@
-import { render, screen, cleanup, getByText } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import userEvent from '@testing-library/user-event'
 import { getPage, initTestHelpers } from 'next-page-tester'
 import { setupServer } from 'msw/node'
 import { handlers } from '../mock/handlers'
